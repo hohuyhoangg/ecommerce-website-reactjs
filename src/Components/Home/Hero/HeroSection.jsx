1aa7877f57828226af5b7182a04a49437b3dfba0
Hồ Huy Hoàng <hoang.ho@katalon.com>
2025-02-05 14:16:48 +0700

    Remove model

--- src/Components/Home/Hero/HeroSection.jsx
+++ src/Components/Home/Hero/HeroSection.jsx
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {OrbitControls} from "@react-three/drei";
 
 import "./HeroSection.css";
-import { Model } from "../../Model/Model";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
-  const [tshirtColor, setTshirtColor] = useState("white");
-
-  const changeColor = (color) => {
-    setTshirtColor(color);
-  };
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -53,27 +46,7 @@ const HeroSection = () => {
               maxPolarAngle={Math.PI / 2}
               minPolarAngle={Math.PI / 2}
             />
-
-            <Model color={tshirtColor} />
           </Canvas>
-          <div className="heroColorBtn">
-            <button
-              onClick={() => changeColor("#353933")}
-              style={{ backgroundColor: "#353933" }}
-            ></button>
-            <button
-              onClick={() => changeColor("#EFBD4E")}
-              style={{ backgroundColor: "#EFBD4E" }}
-            ></button>
-            <button
-              onClick={() => changeColor("#726DE7")}
-              style={{ backgroundColor: "#726DE7" }}
-            ></button>
-            <button
-              onClick={() => changeColor("red")}
-              style={{ backgroundColor: "red" }}
-            ></button>
-          </div>
         </div>
       </div>
     </>
